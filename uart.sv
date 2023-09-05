365d8c5084d41fee12e8260e5087df94d89d5b5c
Tobias Bergkvist <tobias@bergkv.ist>
2023-09-05 22:08:09 +0200

    Make the characters be printed out in the correct order using UART

--- uart.sv
+++ uart.sv
@@ -1,37 +1,41 @@
-module uart_tx(input logic clk, output logic tx);
-    typedef enum logic [1:0] { IDLE = 2'b00, START = 2'b01, DATA = 2'b10, STOP = 2'b11 } state_t;
+module uart_tx(input logic clk, output logic tx = 1);
+    typedef enum logic [1:0] { IDLE = 0, START = 1, DATA = 2, STOP = 3 } state_t;
 
-    state_t state = IDLE;
-    string message = "Hello World!\n";
-    localparam message_length = 13;
-    logic [3:0] char_idx = 0;
-    logic [2:0] bit_cnt = 0;
-    logic [7:0] current_char;
+    localparam message_length = 13*8;
+    logic [message_length-1:0] message = "Hello World! ";
 
-    always_comb current_char = message[char_idx];
-
+    state_t state = IDLE, next_state;
+    logic [10:0] bit_pos = 0, next_bit_pos;
+    logic next_tx;
+    
     always_ff @(posedge clk) begin
-        case(state)
-            IDLE: begin
-                tx <= 1;
-                state <= START;
-                char_idx <= 0;
-            end
-            START: begin
-                tx <= 0;
-                state <= DATA;
-                bit_cnt <= 0;
-            end
-            DATA: begin
-                bit_cnt <= bit_cnt + 1;
-                tx <= current_char[bit_cnt];
-                state <= (bit_cnt == 7) ? STOP : DATA;
-            end
-            STOP: begin
-                tx <= 1;
-                char_idx <= char_idx + 1;
-                state <= (char_idx < message_length) ? START : IDLE;
-            end
-        endcase
+        state <= next_state;
+        bit_pos <= next_bit_pos;
+        tx <= next_tx;
     end
+    
+    always_comb case(state)
+        IDLE: next_bit_pos = message_length - 8;
+        START: next_bit_pos = bit_pos;
+        DATA: begin
+            if (bit_pos === 7) next_bit_pos = message_length - 8;
+            else if (bit_pos % 8 === 7) next_bit_pos = bit_pos - 15;
+            else next_bit_pos = bit_pos + 1;
+        end
+        STOP: next_bit_pos = bit_pos;
+    endcase
+
+    always_comb case(next_state)
+        IDLE: next_tx = 1;
+        START: next_tx = 0;
+        DATA: next_tx = message[next_bit_pos];
+        STOP: next_tx = 1;
+    endcase
+
+    always_comb case(state)
+        IDLE: next_state = START;
+        START: next_state = DATA;
+        DATA: next_state = (bit_pos % 8 === 7) ? STOP : DATA;
+        STOP: next_state = START;
+    endcase
 endmodule
