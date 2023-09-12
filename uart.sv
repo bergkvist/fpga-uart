6b6871489462c8229078e4eafc9078818cfdc0d3
Tobias Bergkvist <tobias@bergkv.ist>
2023-09-12 21:47:45 +0200

    Refactor uart.sv and uart_tb.sv. Shrink signal widths in UartRx, and avoid bitCnt === 8 case

--- uart.sv
+++ uart.sv
@@ -1,27 +1,26 @@
 `timescale 1ns/1ns
 
 module UartTx #(parameter DATA_WIDTH) (
-    input logic clk,
+    input logic uartClk,
     input logic [DATA_WIDTH-1:0] message,
     input logic start,
     output logic tx = 1,
     output logic idle
 );
-    typedef enum logic [1:0] { IDLE, START, DATA, STOP } txState;
-    txState state = IDLE, nextState;
+    typedef enum logic [1:0] { IDLE, START, DATA, STOP } state_t;
+    state_t state = IDLE, nextState;
     logic [10:0] bitPos = DATA_WIDTH - 8, nextBitPos;
     logic nextTx;
-    
+
     always_comb case(state)
         IDLE: idle = 1;
         default: idle = 0;
     endcase
 
     always_comb case(state)
-        IDLE: tx = 1;
         START: tx = 0;
         DATA: tx = message[bitPos];
-        STOP: tx = 1;
+        default: tx = 1;
     endcase
 
     always_comb case(state)
@@ -29,10 +28,10 @@ module UartTx #(parameter DATA_WIDTH) (
         START: nextState = DATA;
         DATA: nextState = (bitPos % 8 === 7) ? STOP : DATA;
         STOP: nextState = (((bitPos === DATA_WIDTH - 8) && (start === 0)) === 1) ? IDLE : START;
+        default: nextState = IDLE;
     endcase
-    
+
     always_comb case(state)
-        IDLE: nextBitPos = DATA_WIDTH - 8;
         START: nextBitPos = bitPos;
         DATA: begin
             if (bitPos === 7) nextBitPos = DATA_WIDTH - 8;
@@ -40,76 +39,65 @@ module UartTx #(parameter DATA_WIDTH) (
             else nextBitPos = bitPos + 1;
         end
         STOP: nextBitPos = bitPos;
+        default: nextBitPos = DATA_WIDTH - 8;
     endcase
 
-    always_ff @(posedge clk) begin
+    always_ff @(posedge uartClk) begin
         state <= nextState;
         bitPos <= nextBitPos;
     end
 endmodule
 
 
-module UartRx #(
-    parameter DATA_WIDTH,
-    parameter BAUD_RATE,
-    parameter CLOCK_RATE
-) (
+module UartRx #(parameter BAUD_RATE, parameter CLOCK_RATE) (
     input logic clk,
     input logic rx,
-    output logic [DATA_WIDTH-1:0] data
+    output logic [7:0] data
 );
-    typedef enum logic [1:0] { IDLE, START, DATA, STOP } rxState;
-    localparam PULSES = CLOCK_RATE / BAUD_RATE;
+    localparam PULSECNT_MAX = CLOCK_RATE / BAUD_RATE;
+    localparam PULSECNT_WIDTH = $clog2(PULSECNT_MAX);
 
-    logic [9:0] pulseCnt = 0, nextPulseCnt;
-    logic [9:0] bitCnt = 0, nextBitCnt;
-    rxState state = IDLE, nextState = IDLE;
+    typedef enum logic [1:0] { IDLE, START, DATA, STOP } state_t;
 
-    logic [DATA_WIDTH-1:0] buffer, nextData;
-    logic rxEdgeDetected = 0;
+    state_t state = IDLE, nextState = IDLE;
+    logic [PULSECNT_WIDTH-1:0] pulseCnt = 0, nextPulseCnt;
+    logic [2:0] bitCnt = 0, nextBitCnt;
+    logic [7:0] buffer, nextData;
     logic startBit;
 
-    logic readyToMeasure;
-    assign readyToMeasure = pulseCnt === PULSES / 2;
+    logic pulseCntMeasure, pulseCntDone;
+    assign pulseCntMeasure = pulseCnt === PULSECNT_MAX / 2;
+    assign pulseCntDone = pulseCnt === PULSECNT_MAX - 1;
 
     always_ff @(posedge clk) begin
-        rxEdgeDetected <= 0;
         state <= nextState;
         bitCnt <= nextBitCnt;
-        pulseCnt <= nextPulseCnt % PULSES;
+        pulseCnt <= nextPulseCnt;
         data <= nextData;
     end
 
-    always_comb case(state)
-        IDLE: begin
-            nextState = (rxEdgeDetected === 1) ? START : IDLE;
-            nextPulseCnt = 0;
-            nextBitCnt = 0;
-        end
-        START: begin
-            nextState = (pulseCnt === PULSES - 1) ? DATA : START;
-            nextPulseCnt = pulseCnt + 1;
-            nextBitCnt = 0;
-            if (readyToMeasure) startBit = rx;
-        end
-        DATA: begin
-            nextState = (bitCnt === DATA_WIDTH) ? STOP : DATA;
-            nextPulseCnt = pulseCnt + 1;
-            nextBitCnt = (bitCnt === DATA_WIDTH) ? 0 : ((pulseCnt === PULSES - 1) ? (bitCnt + 1) : bitCnt);
-            if (readyToMeasure) buffer[bitCnt] = rx;
-        end
-        STOP: begin
-            if (readyToMeasure) begin
-                nextState = (rxEdgeDetected === 1) ? START : IDLE;
-                if (startBit === 0 && rx === 1) nextData = buffer;
-                nextBitCnt = 0;
-                nextPulseCnt = 0;
-            end else begin
-                nextPulseCnt = pulseCnt + 1;
-                nextState = STOP;
-            end
-        end
+    always_ff @(posedge pulseCntMeasure) case(state)
+        START: startBit <= rx;
+        DATA: buffer[bitCnt] <= rx;
+        STOP: if (startBit === 0 && rx === 1) nextData <= buffer;
+        default:;
     endcase
-    
-    always_ff @(negedge rx) rxEdgeDetected <= 1;
+
+    always_comb case(state)
+        IDLE: nextState = (rx === 0) ? START : IDLE;
+        START: nextState = (pulseCntDone === 1) ? DATA : START;
+        DATA: nextState = ((pulseCntDone && bitCnt === 7) === 1) ? STOP : DATA;
+        STOP: nextState = (pulseCntMeasure === 0) ? STOP : IDLE;
+        default: nextState = IDLE;
+    endcase
+
+    always_comb case(state)
+        IDLE: nextPulseCnt = 0;
+        default: nextPulseCnt = (pulseCntDone === 1) ? 0 : (pulseCnt + 1);
+    endcase
+
+    always_comb case(state)
+        DATA: nextBitCnt = (pulseCntDone === 1) ? (bitCnt + 1) : bitCnt;
+        default: nextBitCnt = 0;
+    endcase
 endmodule
--- uart_tb.sv
+++ uart_tb.sv
@@ -1,33 +1,32 @@
 module uart_tb();
+    localparam TX_MESSAGE = {8'h11, 8'h22, 8'h44};
     localparam CLOCK_RATE = 100_000;
     localparam BAUD_RATE = 9600;
-
     initial begin
         $dumpfile("uart.vcd");
         $dumpvars(0, uart_tb);
     end
 
-    logic tx, idle, start = 0;
-    logic clk = 1, uart_clk = 1;
+    logic tx, idle, startTx = 0;
+    logic clk = 1, uartClk = 1;
     initial forever #(0.5s/CLOCK_RATE) clk = ~clk;
-    initial forever #(0.5s/BAUD_RATE) uart_clk = ~uart_clk;
+    initial forever #(0.5s/BAUD_RATE) uartClk = ~uartClk;
 
-    initial begin #100ms start = 1; #150us start = 0; end
+    initial begin
+        #100ms startTx = 1;
+        #150us startTx = 0;
+    end
 
-    UartTx #(.DATA_WIDTH(13*8)) uartTx0(
-        .clk(uart_clk),
-        .message("Hello World! "),
-        .start(start),
+    UartTx #(.DATA_WIDTH($size(TX_MESSAGE))) uartTx0(
+        .uartClk(uartClk),
+        .message(TX_MESSAGE),
+        .start(startTx),
         .tx(tx),
         .idle(idle)
     );
 
     logic [7:0] rxdata;
-    UartRx #(
-        .DATA_WIDTH(8),
-        .BAUD_RATE(BAUD_RATE),
-        .CLOCK_RATE(CLOCK_RATE)
-    ) uartRx0(
+    UartRx #(.BAUD_RATE(BAUD_RATE), .CLOCK_RATE(CLOCK_RATE)) uartRx0(
         .clk(clk),
         .rx(tx),
         .data(rxdata)
